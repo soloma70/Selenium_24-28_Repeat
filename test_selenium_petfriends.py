d8c97d7c1edff746f30f861a88d725966f347235
soloma70 <soloma70@gmail.com>
2021-10-18 22:47:33 +0300

    Добавлена фикстура запуска и закрытия Chrome

--- test_selenium_petfriends.py
+++ test_selenium_petfriends.py
@@ -1,70 +1,49 @@
 # pytest -v --driver Chrome --driver-path C:/1/chromedriver test_selenium_petfriends.py
 
+import pytest
+from selenium import webdriver
 from settings import valid_email, valid_password
 import time
-import pytest
-import uuid
-
-@pytest.hookimpl(hookwrapper=True, tryfirst=True)
-def pytest_runtest_makereport(item, call):
-    # This function helps to detect that some test failed
-    # and pass this information to teardown:
-    outcome = yield
-    rep = outcome.get_result()
-    setattr(item, "rep_" + rep.when, rep)
-    return rep
-
-@pytest.fixture
-def web_browser(request, driver):
-    browser = driver
-    browser.set_window_size(1400, 1000)
-    # Return browser instance to test case:
-    yield browser
-
-    # Do teardown (this code will be executed after each test):
-    if request.node.rep_call.failed:
-        # Make the screen-shot if test failed:
-        try:
-            browser.execute_script("document.body.bgColor = 'white';")
-            # Make screen-shot for local debug:
-            browser.save_screenshot('screenshots/' + str(uuid.uuid4()) + '.png')
-            # For happy debugging:
-            print('URL: ', browser.current_url)
-            print('Browser logs:')
-            for log in browser.get_log('browser'):
-                print(log)
-        except:
-            pass  # just ignore any errors here
 
 
-def test_petfriends(web_browser):
+@pytest.fixture(autouse=True)
+def chrome_setup():
+    '''Фикстура загружает веб-драйвер Хром, меняет размер окна, устанавливает явные и неявные ожидания,
+    страницу авторизации Pet Friends, логинится на сайте, после выполнея основного кода закрывает браузер'''
+    pytest.driver = webdriver.Chrome('C:/1/chromedriver.exe')
+    pytest.driver.set_window_size(1280, 960)
+    pytest.driver.implicitly_wait(10)
+    yield
+    pytest.driver.quit()
+
+def test_petfriends():
     # Открыть базовую страницу PetFriends:
-    web_browser.get("https://petfriends1.herokuapp.com/")
+    pytest.driver.get("https://petfriends1.herokuapp.com/")
 
     time.sleep(2)  # just for demo purposes, do NOT repeat it on real projects!
 
     # Клик на кнопку "Зарегистрироваться"
-    btn_new_user = web_browser.find_element_by_xpath("//button[@onclick=\"document.location='/new_user';\"]")
+    btn_new_user = pytest.driver.find_element_by_xpath("//button[@onclick=\"document.location='/new_user';\"]")
     btn_new_user.click()
 
     # Клик на ссылку "У меня уже есть аккаунт"
-    btn_exist_acc = web_browser.find_element_by_link_text(u"У меня уже есть аккаунт")
+    btn_exist_acc = pytest.driver.find_element_by_link_text(u"У меня уже есть аккаунт")
     btn_exist_acc.click()
 
     # Ввод email
-    field_email = web_browser.find_element_by_id("email")
+    field_email = pytest.driver.find_element_by_id("email")
     field_email.clear()
     field_email.send_keys(valid_email)
 
     # Ввод пароля
-    field_pass = web_browser.find_element_by_id("pass")
+    field_pass = pytest.driver.find_element_by_id("pass")
     field_pass.clear()
     field_pass.send_keys(valid_password)
 
     # Клик на кнопке "Войти"
-    btn_submit = web_browser.find_element_by_xpath("//button[@type='submit']")
+    btn_submit = pytest.driver.find_element_by_xpath("//button[@type='submit']")
     btn_submit.click()
 
     time.sleep(5)  # just for demo purposes, do NOT repeat it on real projects!
 
-    assert  web_browser.current_url == 'https://petfriends1.herokuapp.com/all_pets',"login error"
+    assert  pytest.driver.current_url == 'https://petfriends1.herokuapp.com/all_pets',"login error"
