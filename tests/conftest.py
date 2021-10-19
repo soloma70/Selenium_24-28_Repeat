ea22dd2e1e9b9e61fb6c211dde7600258352fef0
soloma70 <soloma70@gmail.com>
2021-10-19 22:53:05 +0300

    Добавлен conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(autouse=True)
+def driver_setup():
+    '''Фикстура загружает веб-драйвер Хром, меняет размер окна, устанавливает неявное ожидание,
+    после выполнения основного кода закрывает браузер'''
+    pytest.driver = webdriver.Chrome('C:/1/chromedriver.exe')
+    pytest.driver.set_window_size(1280, 960)
+    pytest.driver.implicitly_wait(5)
+    yield
+    pytest.driver.quit()
