87c2dfcd7a8904f41a3c22547ef20efa295537f3
soloma70 <soloma70@gmail.com>
2021-10-19 22:54:23 +0300

    Удален conftest.py

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -5,11 +5,11 @@ import time, pytest
 
 def test_auth_page_with_valid_data():
     page = AuthPage(pytest.driver)
-    time.sleep(3)
+    time.sleep(1)
     page.enter_email(valid_email)
     page.enter_pass(valid_password)
     page.btn_click()
 
     assert page.get_relative_link() == '/all_pets', 'Login error'
 
-    time.sleep(3)
+    time.sleep(2)
