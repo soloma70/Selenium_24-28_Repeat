02c3e9e0e9a89bb26b1a34ebe837ed912c5f8a4c
soloma70 <soloma70@gmail.com>
2021-10-19 22:53:25 +0300

    Удален conftest.py

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -1,17 +1,6 @@
-from selenium import webdriver
 from pages.auth_page import AuthPage
 from settings import valid_email, valid_password
 import time, pytest
-
-@pytest.fixture(autouse=True)
-def driver_setup():
-    '''Фикстура загружает веб-драйвер Хром, меняет размер окна, устанавливает явные, страницу авторизации Pet Friends,
-    логинится на сайте, после выполнея основного кода закрывает браузер'''
-    pytest.driver = webdriver.Chrome('C:/1/chromedriver.exe')
-    pytest.driver.set_window_size(1280, 960)
-    pytest.driver.implicitly_wait(5)
-    yield
-    pytest.driver.quit()
 
 
 def test_auth_page_with_valid_data():
