302941cb36a7a12e52ae1f2e7b5972f09457a81e
soloma70 <soloma70@gmail.com>
2021-10-19 17:01:05 +0300

    Переделан тест на проверку попрадания на главную страницу PetFriends

--- test_selenium_petfriends.py
+++ test_selenium_petfriends.py
@@ -7,43 +7,30 @@ import time
 
 
 @pytest.fixture(autouse=True)
-def chrome_setup():
-    '''Фикстура загружает веб-драйвер Хром, меняет размер окна, устанавливает явные и неявные ожидания,
-    страницу авторизации Pet Friends, логинится на сайте, после выполнея основного кода закрывает браузер'''
+def driver_setup():
+    '''Фикстура загружает веб-драйвер Хром, меняет размер окна, устанавливает явные, страницу авторизации Pet Friends,
+    логинится на сайте, после выполнея основного кода закрывает браузер'''
     pytest.driver = webdriver.Chrome('C:/1/chromedriver.exe')
     pytest.driver.set_window_size(1280, 960)
     pytest.driver.implicitly_wait(10)
+    # Открываем базовую страницу PetFriends:
+    pytest.driver.get("https://petfriends1.herokuapp.com/")
+    # Клик на кнопку "Зарегистрироваться"
+    pytest.driver.find_element_by_xpath("//button[@onclick=\"document.location='/new_user';\"]").click()
+    # Клик на ссылку "У меня уже есть аккаунт"
+    pytest.driver.find_element_by_link_text(u"У меня уже есть аккаунт").click()
     yield
     pytest.driver.quit()
 
-def test_petfriends():
-    # Открыть базовую страницу PetFriends:
-    pytest.driver.get("https://petfriends1.herokuapp.com/")
-
-    time.sleep(2)  # just for demo purposes, do NOT repeat it on real projects!
-
-    # Клик на кнопку "Зарегистрироваться"
-    btn_new_user = pytest.driver.find_element_by_xpath("//button[@onclick=\"document.location='/new_user';\"]")
-    btn_new_user.click()
-
-    # Клик на ссылку "У меня уже есть аккаунт"
-    btn_exist_acc = pytest.driver.find_element_by_link_text(u"У меня уже есть аккаунт")
-    btn_exist_acc.click()
-
+def test_show_petfriends():
     # Ввод email
-    field_email = pytest.driver.find_element_by_id("email")
-    field_email.clear()
-    field_email.send_keys(valid_email)
+    pytest.driver.find_element_by_id('email').send_keys(valid_email)
 
     # Ввод пароля
-    field_pass = pytest.driver.find_element_by_id("pass")
-    field_pass.clear()
-    field_pass.send_keys(valid_password)
-
+    pytest.driver.find_element_by_id('pass').send_keys(valid_password)
     # Клик на кнопке "Войти"
-    btn_submit = pytest.driver.find_element_by_xpath("//button[@type='submit']")
-    btn_submit.click()
+    pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
 
     time.sleep(5)  # just for demo purposes, do NOT repeat it on real projects!
 
-    assert  pytest.driver.current_url == 'https://petfriends1.herokuapp.com/all_pets',"login error"
+    assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends", "login error"
