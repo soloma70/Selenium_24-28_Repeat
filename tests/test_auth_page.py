d72a3cc9516d218b4d125e232fc72e9cb5338814
soloma70 <soloma70@gmail.com>
2021-10-20 11:54:38 +0300

    Убран time.sleep

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -1,15 +1,12 @@
 from pages.auth_page import AuthPage
 from settings import valid_email, valid_password
-import time, pytest
+import pytest
 
 
 def test_auth_page_with_valid_data():
     page = AuthPage(pytest.driver)
-    time.sleep(1)
     page.enter_email(valid_email)
     page.enter_pass(valid_password)
     page.btn_click()
 
     assert page.get_relative_link() == '/all_pets', 'Login error'
-
-    time.sleep(2)
